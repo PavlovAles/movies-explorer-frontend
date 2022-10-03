f7f1b1aa4ce83d9c3377cf423d85d74a96a51179
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-03 14:24:28 +0600

    feat: implement footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <h3 className={styles.footer__heading}>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+      <div className={styles.footer__links}>
+        <p className={styles.footer__copyright}>© 2020</p>
+        <div className={styles['footer__links-container']}>
+          <a className={styles.footer__link} href='https://practicum.yandex.ru/' target='_blank' rel='noreferrer'>Яндекс.Практикум</a>
+          <a className={styles.footer__link} href='https://github.com/PavlovAles/' target='_blank' rel='noreferrer'>Github</a>
+        </div>
+      </div>
+    </footer>
   )
 }
