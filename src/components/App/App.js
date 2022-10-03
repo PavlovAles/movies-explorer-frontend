82f624ad1bf28d42c0dbfd6002811e3c951725c6
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-03 12:12:17 +0600

    feat: add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,34 @@
+import { Route, Switch } from 'react-router-dom';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/signin'>
+          <Register />
+        </Route>
+        <Route path='/signup'>
+          <Login />
+        </Route>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+      </Switch>
     </div>
   );
 }
