7bc1a2e76f4f30b080818896c8d40f3532224de7
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-04 10:27:35 +0600

    feat: burger styles

--- src/components/Header/Navigation/Burger/Burger.js
+++ src/components/Header/Navigation/Burger/Burger.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './Burger.module.css';
 
-export default function Burger() {
+export default function Burger({ clicked, handleClick }) {
   return (
-    <div className={styles.burger}></div>
+    <div className={`${styles.burger} ${clicked ? styles.burger_cross : ''}`} onClick={() => handleClick()}>
+      <div className={styles.burger__dash}></div>
+      <div className={styles.burger__dash}></div>
+      <div className={styles.burger__dash}></div>
+    </div>
   )
 }
