51f74f56ed65554541f081804df077b1ead1608c
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-05 15:43:17 +0600

    fix: promo section on desktop

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Promo() {
   return (
     <section className='promo'>
-      <h1 className='promo__heading'>Учебный проект студента факультета Веб-разработки.</h1>
+      <div className='promo__wrapper'>
+        <h1 className='promo__heading'>Учебный проект студента факультета Веб-разработки.</h1>
+      </div>
     </section>
   )
 }
