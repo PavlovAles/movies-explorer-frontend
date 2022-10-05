dde608fd408e591bce70876894381e7089ae0307
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-05 16:32:01 +0600

    feat: section techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from './About/About'
 import Promo from './Promo/Promo'
+import Techs from './Techs/Techs'
 
 export default function Main() {
   return (
     <>
       <Promo />
       <About />
+      <Techs />
     </>
   )
 }
