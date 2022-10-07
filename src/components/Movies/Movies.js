7c47527b00f10f400ea3cd8c91275e2f411a8004
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-07 16:30:40 +0600

    fix: like size; ref: move preloader, moviesCard/List into ./components

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import MoreButton from './MoreButton/MoreButton'
-import MoviesCardList from './MoviesCardList/MoviesCardList'
-import Preloader from './Preloader/Preloader'
+import MoviesCardList from '../MoviesCardList/MoviesCardList'
+import Preloader from '../Preloader/Preloader'
 import SearchForm from './SearchForm/SearchForm'
 
 export default function Movies({ cards, status }) {
   return (
     <section className='movies'>
       <SearchForm />
-      <MoviesCardList cards={cards} />
+      <MoviesCardList cards={cards} favorite={false} />
       {
         status === 'loading' ?
           <Preloader /> :
---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function MoviesCard({ card, favorite }) {
+  return (
+    <article className='card'>
+      <img className='card__image' src={card.url} alt={card.caption} />
+      <div className='card__caption'>
+        <h4 className='card__title'>
+          {card.name}
+          <p className='card__duration'>{card.duration}</p>
+        </h4>
+        {
+          favorite ?
+            <button className='card__delete'></button> :
+            <button className='card__like'></button>
+        }
+      </div>
+    </article>
+  )
+}
