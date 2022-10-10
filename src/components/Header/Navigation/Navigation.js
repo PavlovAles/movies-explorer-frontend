e6e69cb32006181bccfa76fa247e7ea1e6ba80ea
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-10 19:05:49 +0600

    feat: underline active page; fix: colors

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useWindowWidth from '../../../hooks/useWindowWidth';
 import ProfileButton from '../ProfileButton/ProfileButton';
 import Burger from './Burger/Burger';
@@ -8,6 +8,7 @@ import './Navigation.css';
 export default function Navigation({ authorized }) {
   const [isOpen, setIsOpen] = useState(false);
   const windowWidth = useWindowWidth();
+  const { pathname } = useLocation();
 
   function handleRedirect() {
     setIsOpen(false);
@@ -31,26 +32,36 @@ export default function Navigation({ authorized }) {
       </nav>}
       {authorized && <Burger clicked={isOpen} handleClick={() => setIsOpen(!isOpen)} />}
       {authorized &&
-        <nav
-          className={`navigation__nav-authorized ${isOpen ? 'navigation__nav-authorized_opened' : ''}`}>
+        <nav className={`navigation__nav-authorized ${isOpen ? 'navigation__nav-authorized_opened' : ''}`}>
           <ul className='navigation__list-authorized'>
-            {windowWidth < 768 &&
+            {windowWidth < 769 &&
               <li>
-                <Link className='navigation__link' to='/' onClick={handleRedirect}>
+                <Link
+                  className={`navigation__link ${pathname === '/' ? 'navigation__link_active' : ''}`}
+                  to='/'
+                  onClick={handleRedirect}
+                >
                   Главная
                 </Link>
               </li>}
             <li>
-              <Link className='navigation__link' to='/movies' onClick={handleRedirect}>
+              <Link
+                className={`navigation__link ${pathname === '/movies' ? 'navigation__link_active' : ''}`}
+                to='/movies'
+                onClick={handleRedirect}
+              >
                 Фильмы
               </Link>
             </li>
             <li>
-              <Link className='navigation__link' to='/saved-movies' onClick={handleRedirect}>
+              <Link
+                className={`navigation__link ${pathname === '/saved-movies' ? 'navigation__link_active' : ''}`}
+                to='/saved-movies' 
+                onClick={handleRedirect}>
                 Сохраненные фильмы
               </Link>
             </li>
-            {windowWidth < 768 &&
+            {windowWidth < 769 &&
               <li style={{ marginTop: 'auto' }}>
                 <ProfileButton onClick={handleRedirect} />
               </li>}
