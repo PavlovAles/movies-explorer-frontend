74d8e32940684c4bcb205d0fe3542b308ef21a28
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-11 09:58:09 +0600

    refactor: split navigation in two components

---
+++ src/components/Header/Navigation/AuthorizedNavigation/AuthorizedNavigation.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Burger from '../Burger/Burger';
+import ProfileButton from '../../ProfileButton/ProfileButton';
+import './AuthorizedNavigation.css';
+
+
+export default function AuthorizedNavigation() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  function handleRedirect() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      <Burger clicked={isOpen} handleClick={() => setIsOpen(!isOpen)} />
+      <nav className={`authorized-navigation ${isOpen ? 'authorized-navigation_opened' : ''}`}>
+        <ul className='authorized-navigation__list'>
+          <li className='authorized-navigation__item'>
+            <Link
+              className={`authorized-navigation__link ${pathname === '/' ? 'authorized-navigation__link_active' : ''}`}
+              to='/'
+              onClick={handleRedirect}
+            >
+              Главная
+            </Link>
+          </li>
+          <li className='authorized-navigation__item'>
+            <Link
+              className={`authorized-navigation__link ${pathname === '/movies' ? 'authorized-navigation__link_active' : ''}`}
+              to='/movies'
+              onClick={handleRedirect}
+            >
+              Фильмы
+            </Link>
+          </li>
+          <li className='authorized-navigation__item'>
+            <Link
+              className={`authorized-navigation__link ${pathname === '/saved-movies' ? 'authorized-navigation__link_active' : ''}`}
+              to='/saved-movies'
+              onClick={handleRedirect}>
+              Сохраненные фильмы
+            </Link>
+          </li>
+          <li className='authorized-navigation__item'>
+            <ProfileButton onClick={handleRedirect} />
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,72 +1,16 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import useWindowWidth from '../../../hooks/useWindowWidth';
-import ProfileButton from '../ProfileButton/ProfileButton';
-import Burger from './Burger/Burger';
-import './Navigation.css';
+import React from 'react';
+import AuthorizedNavigation from './AuthorizedNavigation/AuthorizedNavigation';
+import UnauthorizedNavigation from './UnauthorizedNavigation/UnauthorizedNavigation';
 
 export default function Navigation({ authorized }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const windowWidth = useWindowWidth();
-  const { pathname } = useLocation();
-
-  function handleRedirect() {
-    setIsOpen(false);
-  }
 
   return (
     <>
-      {!authorized && <nav>
-        <ul className='navigation__list-unauthorized'>
-          <li>
-            <Link className='navigation__signup' to='/signup'>
-              Регистрация
-            </Link>
-          </li>
-          <li>
-            <Link className='navigation__signin' to='/signin'>
-              Войти
-            </Link>
-          </li>
-        </ul>
-      </nav>}
-      {authorized && <Burger clicked={isOpen} handleClick={() => setIsOpen(!isOpen)} />}
-      {authorized &&
-        <nav className={`navigation__nav-authorized ${isOpen ? 'navigation__nav-authorized_opened' : ''}`}>
-          <ul className='navigation__list-authorized'>
-            {windowWidth < 769 &&
-              <li>
-                <Link
-                  className={`navigation__link ${pathname === '/' ? 'navigation__link_active' : ''}`}
-                  to='/'
-                  onClick={handleRedirect}
-                >
-                  Главная
-                </Link>
-              </li>}
-            <li>
-              <Link
-                className={`navigation__link ${pathname === '/movies' ? 'navigation__link_active' : ''}`}
-                to='/movies'
-                onClick={handleRedirect}
-              >
-                Фильмы
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`navigation__link ${pathname === '/saved-movies' ? 'navigation__link_active' : ''}`}
-                to='/saved-movies' 
-                onClick={handleRedirect}>
-                Сохраненные фильмы
-              </Link>
-            </li>
-            {windowWidth < 769 &&
-              <li style={{ marginTop: 'auto' }}>
-                <ProfileButton onClick={handleRedirect} />
-              </li>}
-          </ul>
-        </nav>}
+      {
+        authorized ?
+          <AuthorizedNavigation /> :
+          <UnauthorizedNavigation />
+      }
     </>
   )
 }
