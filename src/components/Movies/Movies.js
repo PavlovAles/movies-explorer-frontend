3840a1d1ba17c881a50ec67961d291ff0f38b502
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-13 09:43:55 +0600

    feat: add noResult component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MoreButton from './MoreButton/MoreButton';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
 import SearchForm from './SearchForm/SearchForm';
 import './Movies.css';
+import NoResults from './NoResults/NoResults';
 
 export default function Movies({ cards, status }) {
+  const [cardsAmount, setCardsAmount] = useState(3);
+
   return (
     <section className='movies'>
       <SearchForm />
-      <MoviesCardList cards={cards} favorite={false} />
-      {
-        status === 'loading' ?
-          <Preloader /> :
-          <MoreButton />
+      {status === 'loading' && <Preloader />}
+      {status === 'success' &&
+        <>
+          {cards.length ?
+            <MoviesCardList cards={cards} favorite={false} /> :
+            <NoResults />
+          }
+          {(cardsAmount === cards.length) && <MoreButton />}
+        </>
       }
     </section>
   )
