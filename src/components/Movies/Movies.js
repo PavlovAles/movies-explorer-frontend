b42aa791c032ebd1f10f9b2430494350c619da14
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-10-20 10:38:58 +0600

    feat: set initialAmount and cardsToAdd on windowWidth

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import useWindowWidth from '../../hooks/useWindowWidth';
 import MoreButton from './MoreButton/MoreButton';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
@@ -7,10 +8,22 @@ import NoResultsOrError from '../NoResultsOrError/NoResultsOrError';
 import './Movies.css';
 
 export default function Movies({ movies, status, onSearch, onLikeClick }) {
-  const [cardsAmount, setCardsAmount] = useState(3);
+  const [cardsAmount, setCardsAmount] = useState(0);
+  const [cardsToAdd, setCardsToAdd] = useState(0);
+  const windowWidth = useWindowWidth();
+  const prevMovies = useRef();
+
+  useEffect(() => {
+    setCardsToAdd(windowWidth >= 1018 ? 3 : 2);
+    const initialAmount = windowWidth >= 1018 ? 12 : windowWidth > 768 ? 8 : 5;
+    if (movies !== prevMovies.current) {
+      setCardsAmount(Math.min(initialAmount, movies.length));
+    }
+    prevMovies.current = movies;
+  }, [movies, windowWidth]);
 
   function showMoreCards() {
-    setCardsAmount(Math.min(cardsAmount + 3, movies.length));
+    setCardsAmount(Math.min(cardsAmount + cardsToAdd, movies.length));
   }
 
   return (
@@ -20,10 +33,10 @@ export default function Movies({ movies, status, onSearch, onLikeClick }) {
       {status !== 'loading' &&
         <>
           {movies.length ?
-            <MoviesCardList movies={movies} favorite={false} onLikeClick={onLikeClick} /> :
+            <MoviesCardList movies={movies.slice(0, cardsAmount)} favorite={false} onLikeClick={onLikeClick} /> :
             <NoResultsOrError error={status === 'error'} />
           }
-          {(cardsAmount !== movies.length) && <MoreButton onClick={showMoreCards} />}
+          {(cardsAmount < movies.length) && <MoreButton onClick={showMoreCards} />}
         </>
       }
     </section>
